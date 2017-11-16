3a166fc91c4e5f11957409e8582799ee2084d37c
Jaywin <jaywin.huang@gmail.com>
2017-11-15 19:13:17 -0500

    modify run.py to add wsgi

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from devobs import app
+from werkzeug.contrib.fixers import ProxyFix
 
-
-app.run(debug=True)
+if __name__ == '__main__':
+    app.wsgi_app = ProxyFix(app.wsgi_app)
+    app.run(debug=True)
 
