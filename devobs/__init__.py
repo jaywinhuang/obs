abe379f5ee778287cc24299db9b478d4d22d11fc
Jaywin <jaywin.huang@gmail.com>
2017-11-22 21:25:10 -0500

    beautified run.py

--- devobs/__init__.py
+++ devobs/__init__.py
@@ -5,24 +5,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from logging.handlers import RotatingFileHandler
 import logging
+from werkzeug.contrib.fixers import ProxyFix
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.from_object('config')
+
+# Server proxy configuration for Gunicorn.
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 # Logging
 handler = RotatingFileHandler('obs.log', maxBytes=1024*1024, backupCount=20)
 handler.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
+# Manage Logined user.
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-from devobs import views, models
+# Database management
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
+from devobs import views, models
 db.create_all()
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from devobs import app
-from werkzeug.contrib.fixers import ProxyFix
+
 
 if __name__ == '__main__':
-    app.wsgi_app = ProxyFix(app.wsgi_app)
+
     app.run(debug=True)
 
