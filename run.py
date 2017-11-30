d785b10707ff61e7e7a4302f95fef40a05667bda
Jaywin <jaywin.huang@gmail.com>
2017-11-29 23:18:48 -0500

    modify wsgi

--- run.py
+++ run.py
@@ -2,11 +2,11 @@ from devobs import app
 from devobs.apis import apis
 from werkzeug.contrib.fixers import ProxyFix
 
-if __name__ == '__main__':
-    # Add different router modules(Blueprint).
-    # blueprint
-    app.register_blueprint(apis, url_prefix='/api')
+# Add different router modules(Blueprint).
+# blueprint
+app.register_blueprint(apis, url_prefix='/api')
 
+if __name__ == '__main__':
     # Server proxy configuration for Gunicorn.
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(debug=True)
